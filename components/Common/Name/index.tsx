10d2c7087d6d826ab50c08a8cc0125bfd8b50289
Oshoke Oyati <oyatioshoke@yahoo.com>
2023-08-21 14:53:17 +0100

    font weight of name

--- components/Common/Name/index.tsx
+++ components/Common/Name/index.tsx
@@ -5,12 +5,12 @@ const Name = () => {
   return (
     <div>
       <div className=''>
-        <Typography as='h1' weight='font-semibold'>
+        <Typography as='h1' weight='font-bold'>
           OSHOKE OYATI
         </Typography>
       </div>
       <div className='mb-[8px] md:mb-[16px]'>
-        <Typography as='h1' weight='font-semibold'>
+        <Typography as='h1' weight='font-bold'>
           FRONTEND DEVELOPER
         </Typography>
       </div>
