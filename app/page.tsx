44f5109777cf57ee11c0a4cd9ca7bb3e21f802b1
Oshoke Oyati <oyatioshoke@yahoo.com>
2023-09-02 17:58:41 +0100

    smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Container,
   Navigation,
@@ -8,19 +9,33 @@ import {
   Contact,
   Footer,
 } from '@/components';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+import { useLayoutEffect } from 'react';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Home() {
+  useLayoutEffect(() => {
+    ScrollSmoother.create({
+      smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+      smoothTouch: 0.1,
+    });
+  }, []);
   return (
-    <main className='bg-background-100 text-text-100'>
-      <Container>
-        <Navigation />
-        <Hero />
-        <About />
-        <Experience />
-        <Work />
-        <Contact />
-        <Footer />
-      </Container>
+    <main id='smooth-wrapper' className='bg-background-100 text-text-100'>
+      <div id='smooth-content'>
+        <Container>
+          <Navigation />
+          <Hero />
+          <About />
+          <Experience />
+          <Work />
+          <Contact />
+          <Footer />
+        </Container>
+      </div>
     </main>
   );
 }
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Typography, Socials, WAT } from '@/components';
 import Name from '../Common/Name/name';
 import { BiUpArrowAlt } from 'react-icons/bi';
-import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -18,13 +17,13 @@ const Footer = () => {
         </div>
         <Socials />
 
-        <Link href='/#' scroll={false} className='flex  gap-2'>
+        <a href='#' className='flex  gap-2'>
           <Typography as='h6' weight='font-semibold'>
             {' '}
             Back to top
           </Typography>{' '}
           <BiUpArrowAlt className='w-5 h-5 text-black' />{' '}
-        </Link>
+        </a>
       </div>
     </div>
   );
